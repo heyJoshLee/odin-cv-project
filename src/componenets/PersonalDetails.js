2c74f02095ecae31d87f499d476b6537ca6c3348
jleewebdev <lee45276@gmail.com>
2022-08-24 15:15:29 +0800

    Refactor personal info

--- src/componenets/PersonalDetails.js
+++ src/componenets/PersonalDetails.js
@@ -15,26 +15,26 @@ function PersonalDetails() {
     email: '',
   };
 
-  const [personalInfo, setpersonalInfo] = useState(initialFormState);
-  const [editing, setEditing] = useState(true);
-  const dispatch = useDispatch();
+  const personalInfoFromStore = useSelector((state) => state);
 
-  const toggleEdit = () => setEditing(!editing);
+  const [personalInfo, setPersonalInfo] = useState(initialFormState);
+
+  const dispatch = useDispatch();
 
   const handleOnChange = (e) => {
     const keyName = e.target.name;
     const { value } = e.target;
-    setpersonalInfo({
+
+    const newValues = {
       ...personalInfo,
       [keyName]: value,
-    });
+    };
+    setPersonalInfo(newValues);
+    dispatch(setPersonalInformation(newValues));
   };
 
   const handleOnSubmit = (e) => {
     e.preventDefault();
-    console.log(personalInfo);
-    dispatch(setPersonalInformation(personalInfo));
-    toggleEdit();
   };
 
   const renderForm = () => (
@@ -54,51 +54,20 @@ function PersonalDetails() {
       <label htmlFor="email">Email</label>
 
       <input id="email" name="email" placeholder="john@gmail.com" value={personalInfo.email} onChange={handleOnChange} />
-
-      <button>Save</button>
     </form>
   );
 
-  const renderPersonalInformation = () => (
-    <div className="grid-column-2">
-      <div>First Name</div>
-      <div>{personalInfo.firstName}</div>
-      <div>Last Name</div>
-      <div>{personalInfo.lastName}</div>
-      <div>Address</div>
-      <div>{personalInfo.address}</div>
-      <div>Phone Number</div>
-      <div>{personalInfo.phoneNumber}</div>
-      <div>Email</div>
-      <div>{personalInfo.email}</div>
-      <button onClick={() => toggleEdit()}>Edit</button>
-    </div>
+  return (
+    <Collapse
+      header="Personal Details"
+      collapseId="personal-details"
+      headerClass="edit-section-title"
+      bodyClass="edit-section-body"
+      downArrowImagePath={require('../assets/arrow-down.png')}
+    >
+      {renderForm()}
+    </Collapse>
   );
-
-  if (editing) {
-    return (
-      <Collapse
-        header="Personal Details"
-        collapseId="personal-details"
-        headerClass="edit-section-title"
-        bodyClass="edit-section-body"
-        downArrowImagePath={require('../assets/arrow-down.png')}
-      >
-        {renderForm()}
-      </Collapse>
-    );
-  }
-
-  if (!editing) {
-    return (
-      <div className="edit-section">
-        <div className="edit-section-title">Personal Details</div>
-        <div className="edit-section-body">
-          {renderPersonalInformation()}
-        </div>
-      </div>
-    );
-  }
 }
 
 export default PersonalDetails;
